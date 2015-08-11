f97d73e66cba6fa07e3f8d9a93092d2e20071887
Hakan Tandogan <hakan@tandogan.com>
2015-08-11 17:50:23 +0200

    Kickstart spring-boot

---
+++ src/main/java/com/gurkensalat/chronos/Application.java
@@ -0,0 +1,19 @@
+package com.gurkensalat.chronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(Application.class);
+    }
+}
