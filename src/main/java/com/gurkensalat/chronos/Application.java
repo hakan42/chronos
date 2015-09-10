e699a62bf79fa355eebc2ab9af8fc869625c17db
Hakan Tandogan <hakan@tandogan.com>
2015-09-10 18:33:38 +0200

    Make frontend translatable

--- src/main/java/com/gurkensalat/chronos/Application.java
+++ src/main/java/com/gurkensalat/chronos/Application.java
@@ -1,8 +1,11 @@
 package com.gurkensalat.chronos;
 
+import org.apache.commons.lang3.CharEncoding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Application
@@ -15,5 +18,15 @@ public class Application
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
     {
         return application.sources(Application.class);
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource()
+    {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setDefaultEncoding(CharEncoding.UTF_8);
+        return messageSource;
     }
 }
