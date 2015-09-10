7372942184aa46a96ca68be6d5a92d1ec1b15440
Hakan Tandogan <hakan@tandogan.com>
2015-09-10 15:43:42 +0200

    Pass arguments to application, might come in handy

--- src/main/java/com/gurkensalat/chronos/Application.java
+++ src/main/java/com/gurkensalat/chronos/Application.java
@@ -9,7 +9,7 @@ public class Application
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
