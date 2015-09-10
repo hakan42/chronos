64bcc1eb0b1d6087fef2aa603d0f56300ad2447e
Hakan Tandogan <hakan@tandogan.com>
2015-09-10 21:20:11 +0200

    Externally stoppable runner bean

---
+++ src/main/java/com/gurkensalat/chronos/LayerListRunner.java
@@ -0,0 +1,45 @@
+package com.gurkensalat.chronos;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ApplicationContextEvent;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LayerListRunner implements CommandLineRunner, ApplicationListener<ApplicationContextEvent>
+{
+    private final static Logger LOGGER = LoggerFactory.getLogger(LayerListRunner.class);
+
+    private static boolean running = true;
+
+    public void run(String... args) throws Exception
+    {
+        LOGGER.info("Running my additional runner");
+
+        while (running)
+        {
+            LOGGER.info("Doing some work...");
+
+            Thread.sleep(15 * 1000);
+        }
+
+        LOGGER.info("Finished my additional runner");
+    }
+
+    public void onApplicationEvent(ApplicationContextEvent event)
+    {
+        LOGGER.info("Context received event {}", event);
+        if (event instanceof ContextStoppedEvent || event instanceof ContextClosedEvent)
+        {
+            // No clue why onApplicationEvent(ContextStoppedEvent) is not called, fake it here...
+            running = false;
+            LOGGER.info("Set running to {}", running);
+        }
+    }
+}
---
+++ src/main/java/com/gurkensalat/chronos/LedLayer.java
@@ -0,0 +1,8 @@
+package com.gurkensalat.chronos;
+
+/**
+ * Created by hakan on 10.09.15.
+ */
+public interface LedLayer
+{
+}
