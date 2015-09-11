903774c22539470714d8f7b63758729c481e3436
Hakan Tandogan <hakan@tandogan.com>
2015-09-12 00:12:09 +0200

    Actually working chase layer

--- src/main/java/com/gurkensalat/chronos/ChaseLayer.java
+++ src/main/java/com/gurkensalat/chronos/ChaseLayer.java
@@ -1,5 +1,6 @@
 package com.gurkensalat.chronos;
 
+import opc.Animation;
 import opc.PixelStrip;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,68 +9,33 @@ public class ChaseLayer implements LedLayer
 {
     private final static Logger LOGGER = LoggerFactory.getLogger(ChaseLayer.class);
 
-    /**
-     * @formatter:off
-     #!/usr/bin/env python
-
-     # Light each LED in sequence, and repeat.
-
-     import opc, time, random
-
-     numLEDs = 60
-     client = opc.Client('127.0.0.1:7890')
-
-     while True:
-     for i in range(numLEDs):
-     pixels = [ (0,0,0) ] * numLEDs
-     pixels[0] = (128, 0, 0)
-     pixels[i] = (0, random.randint(0, 256), random.randint(0, 256))
-     client.put_pixels(pixels)
-     time.sleep(0.1)
-     * @formatter:on
-     */
-
-     /*
-      * @formatter:off
-      *
-      * int color = 0xFF0000;  // red
-      * strip.setPixelColor(3, color);
-      * strip.setPixelColor(5, 0x888800); // yellow
-      * strip.setPixelColor(7, 0x00FF00); // green
-      *
-      * server.show();        // Display the pixel changes
-      * Thread.sleep(5000);   // Wait five seconds
-      *
-      * @formatter:on
-      */
-
     private static int currentLed = 0;
 
     public void prepare(PixelStrip strip)
     {
+        currentLed++;
         if (currentLed >= 60)
         {
             currentLed = 0;
         }
 
-        for (int i = 1; i < 60; i++)
+        for (int i = 0; i < 60; i++)
         {
-            if (i == 0)
+            int color = Animation.BLACK;
+            if (i == 0 || i == 15 || i == 30 || i == 45)
             {
-                strip.setPixelColor(i, 0xFF0000);
+                color = Animation.makeColor(0xff, 0x00, 0x00);
             }
             else if (i == currentLed)
             {
-                int r = 0;
-                int g = 42;
-                int b = 42;
+                int r = (int)(Math.random() * 256);
+                int g = (int)(Math.random() * 256);
+                int b = (int)(Math.random() * 256);
 
-                strip.setPixelColor(i, r * 256 * 256 + g * 256 + b);
+                color = Animation.makeColor(r, g, b);
             }
-            else
-            {
-                strip.setPixelColor(i, 0x000000);
-            }
+
+            strip.setPixelColor(i, color);
         }
     }
 
