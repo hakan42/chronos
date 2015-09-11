d81868c5ce9186826f18a0ba883f4c9c40085b24
Hakan Tandogan <hakan@tandogan.com>
2015-09-11 16:29:23 +0200

    Quick-And-Dirty reimplementation of python chase example

--- src/main/java/com/gurkensalat/chronos/ChaseLayer.java
+++ src/main/java/com/gurkensalat/chronos/ChaseLayer.java
@@ -1,7 +1,13 @@
 package com.gurkensalat.chronos;
 
-public class ChaseLayer
+import opc.PixelStrip;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChaseLayer implements LedLayer
 {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ChaseLayer.class);
+
     /**
      * @formatter:off
      #!/usr/bin/env python
@@ -22,4 +28,49 @@ public class ChaseLayer
      time.sleep(0.1)
      * @formatter:on
      */
+
+     /*
+      * @formatter:off
+      *
+      * int color = 0xFF0000;  // red
+      * strip.setPixelColor(3, color);
+      * strip.setPixelColor(5, 0x888800); // yellow
+      * strip.setPixelColor(7, 0x00FF00); // green
+      *
+      * server.show();        // Display the pixel changes
+      * Thread.sleep(5000);   // Wait five seconds
+      *
+      * @formatter:on
+      */
+
+    private static int currentLed = 0;
+
+    public void prepare(PixelStrip strip)
+    {
+        if (currentLed >= 60)
+        {
+            currentLed = 0;
+        }
+
+        for (int i = 1; i < 60; i++)
+        {
+            if (i == 0)
+            {
+                strip.setPixelColor(i, 0xFF0000);
+            }
+            else if (i == currentLed)
+            {
+                int r = 0;
+                int g = 42;
+                int b = 42;
+
+                strip.setPixelColor(i, r * 256 * 256 + g * 256 + b);
+            }
+            else
+            {
+                strip.setPixelColor(i, 0x000000);
+            }
+        }
+    }
+
 }
