d9de27b1ce44d02e89164899d85116684a39aa43
Hakan Tandogan <hakan@tandogan.com>
2015-09-12 22:43:08 +0200

    Typo in private attribute name

---
+++ src/main/java/com/gurkensalat/chronos/SecondHandLayer.java
@@ -0,0 +1,34 @@
+package com.gurkensalat.chronos;
+
+import opc.PixelStrip;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SecondHandLayer extends AbstractLedLayer implements LedLayer
+{
+    private final static Logger LOGGER = LoggerFactory.getLogger(SecondHandLayer.class);
+
+    // TODO make this configurable
+    private int secondHandColor = BLUE;
+
+    // For unit testing...
+    private int pixelNumber;
+
+    public void prepare(PixelStrip strip, DateTime now)
+    {
+        int minute = now.minuteOfHour().get();
+
+        pixelNumber = minute;
+        LOGGER.debug("Now is {} -> pixel is {}", minute, pixelNumber);
+
+
+        strip.setPixelColor(pixelNumber, secondHandColor);
+    }
+
+    // For unit testing...
+    protected int getHourHandPixelNumber()
+    {
+        return pixelNumber;
+    }
+}
