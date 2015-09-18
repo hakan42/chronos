ec1c253986a811604d9f94201ff0cd673ace8413
Hakan Tandogan <hakan@tandogan.com>
2015-09-18 17:50:16 +0200

    Make the ConfigController a proper REST controller too

--- src/main/java/com/gurkensalat/chronos/ConfigContoller.java
+++ src/main/java/com/gurkensalat/chronos/ConfigContoller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.io.BufferedWriter;
@@ -20,6 +21,8 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 @Controller
+@RestController
+@RequestMapping("/rest")
 public class ConfigContoller
 {
     private final static Logger LOGGER = LoggerFactory.getLogger(ConfigContoller.class);
