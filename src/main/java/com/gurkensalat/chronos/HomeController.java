cb974e5a8e2189ac9a04be8fd1e45ddd1142b908
Hakan Tandogan <hakan@tandogan.com>
2015-12-07 18:12:10 +0100

    Color choosers for all LED layers

--- src/main/java/com/gurkensalat/chronos/HomeController.java
+++ src/main/java/com/gurkensalat/chronos/HomeController.java
@@ -6,14 +6,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
+
 @Controller
 class HomeController
 {
     private final static Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
+    @Resource(name = "hourHandLayer")
+    private LedLayer hourHandLayer;
+
+    @Resource(name = "minuteHandLayer")
+    private LedLayer minuteHandLayer;
+
+    @Resource(name = "secondHandLayer")
+    private LedLayer secondHandLayer;
+
+    @Resource(name = "mainHoursLayer")
+    private LedLayer mainHoursLayer;
+
+    @Resource(name = "secondaryHoursLayer")
+    private LedLayer secondaryHoursLayer;
+
     @RequestMapping("/")
-    String index(Model model)
+    public String index(Model model)
     {
+        model.addAttribute("hourHand", hourHandLayer);
+        model.addAttribute("minuteHand", minuteHandLayer);
+        model.addAttribute("secondHand", secondHandLayer);
+
+        model.addAttribute("mainHours", mainHoursLayer);
+        model.addAttribute("secondaryHours", secondaryHoursLayer);
+
         return "index";
     }
 }
